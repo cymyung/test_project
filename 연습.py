a5b819c61b5c5fbd6923ab09497ff87a6fe4f556
cymyung <cymyung@naver.com>
2024-05-08 10:41:46 +0900

    first code

---
+++ 연습.py
@@ -0,0 +1,15 @@
+1+1
+3+3
+
+for i in range(0,5):
+    print(i)
+
+
+import pandas as pd
+
+df = pd.DataFrame([[1,2,3,4],
+                   [2,3,4,5]])
+df
+df1 = pd.DataFrame([[1,2,3,4],
+                   [2,3,4,5]])
+df1
